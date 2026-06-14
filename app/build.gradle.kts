6b8c72e133bfb2eae600720e92f71592a18c1cd5
elonj <56976129+elonj@users.noreply.github.com>
2026-06-14 16:33:06 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.kirin.bilitv"
     minSdk = 23
     targetSdk = 36
-    versionCode = 101
-    versionName = "1.0.1"
+    versionCode = 102
+    versionName = "1.0.2"
 
     ndk {
       abiFilters.clear()
